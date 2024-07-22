a2bd33c03c645d219645ae2d1049ae14db866a9f
Mihai Popovici <popovicimihaimail@gmail.com>
2024-07-22 21:00:47 +0300

    Fix no feeback after clicking project card

--- src/components/admin/projects/ProjectCard.tsx
+++ src/components/admin/projects/ProjectCard.tsx
@@ -4,36 +4,38 @@ import { InferSelectModel } from "drizzle-orm";
 import Image from "next/image";
 import placeholder from "../../../../public/placeholder.svg";
 import {UsersProjects} from "@/db/schema";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 type Props = {
   project: InferSelectModel<typeof UsersProjects>
 }
 
 export default function ProjectCard({project}:Props){
-  const router = useRouter();
   return (
-    <Card
-    className="overflow-hidden cursor-pointer"
-    onClick={()=>{router.push('/admin/projects/'+project.id)}}
-  >
-    <CardHeader>
-      <CardTitle className="line-clamp-1">{project.title}</CardTitle>
-      <CardDescription className="line-clamp-2">
-        {project.description}
-      </CardDescription>
-    </CardHeader>
-    <CardContent>
-      <div className="grid gap-2">
-        <Image
-          alt="Product image"
-          className="aspect-square w-full rounded-md object-cover"
-          height="300"
-          src={project.thumbnailUrl || placeholder}
-          width="300"
-        />
-      </div>
-    </CardContent>
-  </Card>
+    <form action={()=>redirect('/admin/projects/'+project.id)}>
+      <button type="submit" className="text-start">
+        <Card
+        className="overflow-hidden cursor-pointer"
+        >
+          <CardHeader>
+            <CardTitle className="line-clamp-1">{project.title}</CardTitle>
+            <CardDescription className="line-clamp-2">
+              {project.description}
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid gap-2">
+              <Image
+                alt="Product image"
+                className="aspect-square w-full rounded-md object-cover"
+                height="300"
+                src={project.thumbnailUrl || placeholder}
+                width="300"
+              />
+            </div>
+          </CardContent>
+        </Card>
+      </button>
+    </form>
   );
 }
