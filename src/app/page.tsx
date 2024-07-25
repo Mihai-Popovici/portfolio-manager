5b2f77fd1f0506bf144b502f6cad11c33a7427e9
Mihai Popovici <popovicimihaimail@gmail.com>
2024-07-25 16:52:10 +0300

    Home Page new design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { db } from "@/db";
 import { UsersProjects } from "@/db/schema";
 import { desc } from "drizzle-orm";
-import { Instagram, Linkedin } from "lucide-react";
+import { ArrowDown, Instagram, Linkedin } from "lucide-react";
 import Image from "next/image";
 import X from "../../public/x_logo.svg";
 
@@ -10,16 +10,21 @@ export default async function Home() {
   const ownerName = process.env.NEXT_PUBLIC_OWNER_NAME;
   return (
     <>
-      <div className="w-full relative bg-black text-white">
-        <video className="w-full" muted autoPlay loop>
+      <div className="w-full h-screen relative bg-black text-white overflow-hidden">
+        <div className="absolute z-20 bottom-0 mb-5 w-full border-b border-white bg-transparent">
+          <p className="text-white flex gap-2 text-xl p-5">SCROLL <ArrowDown className="animate-bounce"/></p>
+        </div>
+        <video className="max-w-none absolute left-1/2 -translate-x-1/2 w-screen h-screen object-cover" muted autoPlay loop>
           <source src="https://portfolio-manager.s3.tebi.io/videoplayback.mp4"></source>
         </video>
-        <div className="absolute bottom-2 md:top-1/2 md:-translate-y-1/2 ml-2 md:ml-10 mb-0 md:mb-2">
-          <h1 className="relative bg-black/10 backdrop-blur-md p-3 text-xl md:text-6xl my-0 skew-x-12 border-b md:border-b-2 border-green-400">{ownerName}</h1>
-          <h2 className="relative h-[52px] md:h-[84px] bg-black/10 backdrop-blur-md p-3 text-lg md:text-4xl my-0 -skew-x-12 border-t md:border-t-2 border-green-400">3D Modeling and Animation</h2>
+        <div className="absolute left-0 bottom-60 bg-white/70 rounded-tr-[2rem] rounded-br-[2rem] text-black italic pt-8 pl-16 pb-2 pr-16">
+          <h1 className="leading-[20px] md:leading-[40px] text-2xl md:text-5xl mb-0">3D PRODUCT<br/>VISUALIZATION</h1>
+          <h2 className="text-base md:text-2xl mt-0">BY {ownerName?.toUpperCase()}</h2>
+          {/* <h1 className="relative bg-black/10 backdrop-blur-md p-3 text-xl md:text-6xl my-0 skew-x-12 border-b md:border-b-2 border-green-400">{ownerName}</h1>
+          <h2 className="relative h-[52px] md:h-[84px] bg-black/10 backdrop-blur-md p-3 text-lg md:text-4xl my-0 -skew-x-12 border-t md:border-t-2 border-green-400">3D Modeling and Animation</h2> */}
         </div>
       </div>
-      <div className="sticky z-20 h-16 md:h-24 w-full top-0  backdrop:blur-lg flex justify-center gap-10 md:gap-20 items-center text-black backdrop-blur-[8px] mask-tb">
+      <div className="hidden sticky z-20 h-16 md:h-24 w-full top-0  backdrop:blur-lg flex justify-center gap-10 md:gap-20 items-center text-black backdrop-blur-[8px] mask-tb">
         <div className="relative shadow-lg rounded-full">
           <div className="w-10 md:w-16 h-10 md:h-16 bg-[#0a66c2] rounded-full">
           </div>
@@ -42,10 +47,22 @@ export default async function Home() {
           </a>
         </div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2">
+      <div className="w-full flex flex-col lg:flex-row justify-center py-10">
+        <div className="w-full lg:w-1/2 flex justify-end">
+          <h3 className="h-min text-4xl md:text-7xl italic w-[80%] py-2 pl-5 pr-20 bg-black text-white leading-[28px!important] md:leading-[60px!important]">DESIGNED <br/>TO <b>DELIGHT</b></h3>
+        </div>
+        <div className="w-full lg:w-1/2 flex flex-col gap-5 justify-start pl-5 pr-20">
+          <p className="text-end text-lg md:text-xl">Are you launching a <b>new product</b> and need visuals to get people as excited about it as you are? I got you.</p>
+          <div className="w-full max-w-[1200px] flex justify-end">
+            <button className="w-fit italic px-8 py-4 text-4xl bg-orange-600 hover:bg-black text-white font-bold">TALK TO DAVID</button>
+          </div>
+          <p className="text-end text-sm md:text-base text-slate-500">(this button will allow you to <b>set up a call with me</b>)</p>
+        </div>       
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 overflow-hidden">
         {(projects.map((project)=>(
-          <a key={project.id} href={'/'+project.slug} className="group h-[300px] hover:border-8 border-white hover:outline hover:outline-4 outline-green-400 overflow-hidden relative transition-all m-[2px]">
-            <h3 className="z-10 absolute w-full text-center top-1/2 text-xs md:text-2xl text-white drop-shadow-[0_5px_5px_rgba(0,0,0,0.8)] transition-all group-hover:top-1/3 group-hover:text-sm md:group-hover:text-4xl">{project.title}</h3>
+          <a key={project.id} href={'/'+project.slug} className="relative group h-[300px] ">
+            <h3 className="hidden group-hover:flex z-10 absolute w-full h-full top-0 left-0 bg-black/50 justify-center items-center text-xs md:text-2xl text-white">{project.title}</h3>
             <Image className="w-full h-full object-cover" src={project.thumbnailUrl || ''} width={1000} height={1000} alt="Thumbnail"></Image>
           </a>
         )))}
